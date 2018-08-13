7d708c1b1587f5db5a1f9b5ac3b198a2f1b44306
Rafał <40712810+LuckyCode7@users.noreply.github.com>
2018-08-13 19:02:44 +0200

    Update balanceTests.cpp

--- test/balanceTests.cpp
+++ test/balanceTests.cpp
@@ -1,7 +1,20 @@
 #include <gtest/gtest.h>
 #include "../src/balance.hpp"
 
-TEST(balance, check)
+TEST(balance, correct)
 {
     EXPECT_EQ(balance("()"), 1);
+    EXPECT_EQ(balance("({})"), 1);
+    EXPECT_EQ(balance("[({})]"), 1);
+    EXPECT_EQ(balance("([])"), 1);
+    EXPECT_EQ(balance("()[]"), 1);
+    EXPECT_EQ(balance("{}()"), 1);
+}
+
+TEST(balance, incorrect)
+{
+    EXPECT_EQ(balance(""), 0);
+    EXPECT_EQ(balance("("), 0);
+    EXPECT_EQ(balance(")"), 0);
+    EXPECT_EQ(balance("()["), 0);
 }
