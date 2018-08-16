3b14e40e849503d9be00bd2ce45c23d516da5d59
Rafał Nowak <nowak9.private@gmail.com>
2018-08-16 12:44:23 +0200

    Added new test

--- test_catch/balanceTests.cpp
+++ test_catch/balanceTests.cpp
@@ -1,4 +1,18 @@
 #include <gtest/gtest.h>
 #include "../inc/balance.hpp"
 
+SCENARIO("Bracket balance test")
+    {
+        GIVEN("empty string")
+        {
+            std::string str = "";
+            WHEN("call function balance")
+            {
+                THEN("Expect false")
+                {
+                    REQUIRE_FALSE(balance(str));
+                }
+            }
+        }
+    }
 
